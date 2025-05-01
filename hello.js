69c5eeffda5323b89603c97aa5a935c294934c7e
golu-aarya <aaryagolu1@gmail.com>
2025-05-01 16:29:37 -0700

    add hello.js and index.html

---
+++ hello.js
@@ -0,0 +1,10 @@
+console.log("golu") // this has a special meaning
+
+// user -> name, email, phone and city and i have to store 
+const accountNumber = 4036287890;
+let accountEmail = "therahulkumar.dev@gmail.com";
+var accountPassword = "@rk.dev";
+accountCity = "Patna";  // Not recommended
+let accountState;
+// error
+// accountNumber = 4829292902; // not allowed
