c52dce53c77b4c527ce0095028f0c41605201a6d
golu-aarya <aaryagolu1@gmail.com>
2025-05-06 16:22:53 -0700

    Add all files again

---
+++ practice/script.js
@@ -0,0 +1,21 @@
+// document.querySelector('#title').innerText = "welcome"
+
+// document.getElementById("box").style.backgroundColor = "blue"
+
+// let randomNumber = Math.floor(Math.random() * 10) + 1
+
+// alert(randomNumber);
+
+// function checkEvenOdd(){
+//     const userInput = document.getElementById("number").value;
+//     if(userInput % 2 == 0){
+//         alert("Even")
+//     }else{
+//         alert("Odd")
+//     }
+// }
+
+setInterval(() => {
+   console.log("Running....");
+   
+}, 1000);                                                                                                                                       
