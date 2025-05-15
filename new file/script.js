7fe237895e837811864e9950416f31603960acdf
golu-aarya <aaryagolu1@gmail.com>
2025-05-15 12:25:11 -0700

    counter app

--- new file/script.js
+++ new file/script.js
@@ -1,6 +1,29 @@
-const h1 = document.getElementsByTagName('h1');
-console.log(h1)
+// const h1 = document.getElementsByTagName('h1');
+// console.log(h1)
 
-h1[0].style.textAlign = 'center'
-h1[0].style.color = 'green'
-h1[0].style.backgroundColor = 'yellow'
+// h1[0].style.textAlign = 'center'
+// h1[0].style.color = 'green'
+// h1[0].style.backgroundColor = 'yellow' 
+
+const events = [
+    "javascript",
+    "python",
+   "html",
+   "css",
+   "rect",
+   "jeson"
+]
+
+const eventList = document.getElementById("eventList")
+const element1 = document.createElement("li")
+
+// element1.innerText = events[0]
+// eventList.appendChild(element1)
+
+events.forEach(event => {
+    const element = document.createElement("li")
+    element.innerText = event
+    // console.log(element)
+
+    eventList.appendChild(element)
+})
